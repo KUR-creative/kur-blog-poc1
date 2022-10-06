6f404f4052848ae7b7d118d72d2fb4f2fa24e475
kur <rhdnfka94@gmail.com>
2022-10-06 12:20:04 +0900

    Change alias: pub instead of p

--- test/unit/kur/blog/publishable_test.clj
+++ test/unit/kur/blog/publishable_test.clj
@@ -1,14 +1,14 @@
 (ns unit.kur.blog.publishable-test
   (:require
    [clojure.test :refer [is deftest run-tests]]
-   [kur.blog.publishable :as p]
+   [kur.blog.publishable :as pub]
    [kur.blog.post :as post]
    [kur.blog.resource :as resource]))
 
 (deftest ctor-test
-  (is (some? (p/publishable post/post
-                            "test/fixture/blog-md/kur2207281052.+.md"
-                            "test/fixture/tmp-html/")))
-  (is (some? (p/publishable resource/resource
-                            "test/fixture/blog-md/kur2207281052.+.md"
-                            "test/fixture/tmp-html/"))))
+  (is (some? (pub/publishable post/post
+                              "test/fixture/blog-md/kur2207281052.+.md"
+                              "test/fixture/tmp-html/")))
+  (is (some? (pub/publishable resource/resource
+                              "test/fixture/blog-md/kur2207281052.+.md"
+                              "test/fixture/tmp-html/"))))
