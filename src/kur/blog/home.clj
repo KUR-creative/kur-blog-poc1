fccc63ef7afc369f1bace4f1ff514c4f08733005
kur <rhdnfka94@gmail.com>
2022-12-26 08:52:38 +0900

    Add Publishable: home

---
+++ src/kur/blog/home.clj
@@ -0,0 +1,18 @@
+(ns kur.blog.home
+  (:require [babashka.fs :as fs]
+            [kur.blog.publishable :refer [Publishable]]))
+
+(defn html-file-path [html-dir] ; policy
+  (str (fs/path html-dir "home.html")))
+
+(defrecord Home [id html-dir]
+  Publishable
+  (out-form [this] (:html-path this))
+  (public? [this] true) ; NOTE: state는 이미 먼저 upd 되어 있음을 가정
+  (update! [this state]
+    ;(->> md-path slurp (page-post/html nil) (spit html-path))
+    ; page-home/html this ..?
+    (assoc this :html-path (html-file-path html-dir))))
+
+(defn home [_ html-dir] (->Home "home" html-dir))
+;(kur.blog.publishable/update! (->Home "home" "test"))
