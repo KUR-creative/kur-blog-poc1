2000779cc3c7fd77c43f9120efe2e5668db8cbb4
kur <rhdnfka94@gmail.com>
2023-01-04 15:13:42 +0900

    Fix main to compilable
    기존 코드는 컴파일 시(tools.build/compile-clj) kur.blog.home.Home을
    kur.blog.publishable.Publishable로 cast할 수 없다는 에러가 발생한다.
    
    아마도 현재 init 코드에 해당하는 부분이 AOT 컴파일 시 실행이 불가능한
    코드가 있는 것으로 사료된다. 그래서 ring handler인 app에는 nil로
    초기화된 atom인 state를 전달하고, state를 init에서 초기화(즉 런타임에
    state를 초기화)하게 하였다.

--- src/kur/blog/main.clj
+++ src/kur/blog/main.clj
@@ -5,16 +5,22 @@
             [kur.blog.publisher :as publisher])
   (:gen-class))
 
-(def app
+(def state (atom nil))
+
+(defn init [_]
   (let [cfg-path "resource/config/static-test.edn"
-        s (state/state (load-config :file cfg-path))
-        state (atom (update-vals s #(pub/update! % s)))
-        ;; 현재 pub update!에 순서가 없어도 되서 문제되지 않음
-        ;; 만일 update!에 순서가 필요하다면 update-vals말고 직접 짜야 함
-        ]
-    (publisher/publisher state)))
+        s (state/state (load-config :file cfg-path))]
+    (reset! state (update-vals s #(pub/update! % s)))))
+;; 현재 pub update!에 순서가 없어도 되서 문제되지 않음
+;; 만일 update!에 순서가 필요하다면 update-vals말고 직접 짜야 함
+;;
+;; 이후 update! 부분을 빼거나(updater / publisher 구분)
+;; nginx에서 shared hash-map인가.. 써서 최적화 할 수도 있긴 할 듯??
+
+(def app (publisher/publisher state)) ;; the handler
 
 (defn -main [_]
+  (init nil)
   (publisher/start! app
                     (load-config :file "resource/config/static-test.edn")))
 
